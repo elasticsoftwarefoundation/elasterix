83ce5ca68b6632bc561e9496b3a0d0c4f0e80baf
Joost van de Wijgerd <jwijgerd@gmail.com>
2013-04-11 13:20:08 +0200

    now using elasticactors-base so removed copied jackson serializers and have ElasterixServer extends SpringBasedActorSystem class

--- server/src/main/java/org/elasterix/server/ElasterixServer.java
+++ server/src/main/java/org/elasterix/server/ElasterixServer.java
@@ -16,36 +16,10 @@
 
 package org.elasterix.server;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.Version;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.module.SimpleModule;
-import org.elasterix.elasticactors.ActorRef;
-import org.elasterix.elasticactors.ActorState;
 import org.elasterix.elasticactors.ActorSystem;
-import org.elasterix.elasticactors.ActorSystemBootstrapper;
-import org.elasterix.elasticactors.ActorSystemConfiguration;
-import org.elasterix.elasticactors.ElasticActor;
-import org.elasterix.elasticactors.serialization.Deserializer;
-import org.elasterix.elasticactors.serialization.MessageDeserializer;
-import org.elasterix.elasticactors.serialization.MessageSerializer;
-import org.elasterix.elasticactors.serialization.Serializer;
-import org.elasterix.server.actors.Device;
-import org.elasterix.server.actors.User;
-import org.elasterix.server.serialization.JacksonActorRefDeserializer;
-import org.elasterix.server.serialization.JacksonActorRefSerializer;
-import org.elasterix.server.serialization.JacksonActorStateDeserializer;
-import org.elasterix.server.serialization.JacksonActorStateSerializer;
-import org.elasterix.server.serialization.JacksonMessageDeserializer;
-import org.elasterix.server.serialization.JacksonMessageSerializer;
-import org.elasterix.server.sip.SipMessageHandlerImpl;
-import org.elasterix.sip.SipMessageHandler;
-import org.elasterix.sip.SipMessageSender;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.StringUtils;
+import org.elasticsoftwarefoundation.elasticactors.base.SpringBasedActorSystem;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Elasterix Implementation of the Elastic Actor Framework
@@ -54,31 +28,13 @@ import org.springframework.util.StringUtils;
  * @author Joost van de Wijgerd
  * @author Leonard Wolters
  */
-public class ElasterixServer implements ActorSystemConfiguration, ActorSystemBootstrapper {
+public class ElasterixServer extends SpringBasedActorSystem {
 	private static final Logger log = Logger.getLogger(ElasterixServer.class);
 
 	private final String name;
 	private final int numberOfShards;
-	private final ObjectMapper objectMapper = new ObjectMapper();
-	private final Serializer<ActorState, byte[]> actorStateSerializer = new JacksonActorStateSerializer(objectMapper);
-	private final Deserializer<byte[], ActorState> actorStateDeserializer = new JacksonActorStateDeserializer(objectMapper);
 
-	@Autowired
-	private SipMessageSender sipMessageSender;
 
-	/** Sip Message Handler needs to be manually created due to reference to Actor System */
-	private SipMessageHandler sipMessageHandler;
-	
-	private final Map<Class<?>, MessageSerializer<?>> messageSerializers = new HashMap<Class<?>, MessageSerializer<?>>() {{
-		put(User.class, new JacksonMessageSerializer<User>(objectMapper));
-		put(Device.class, new JacksonMessageSerializer<Device>(objectMapper));
-	}};
-
-	private final Map<Class<?>, MessageDeserializer<?>> messageDeserializers = new HashMap<Class<?>, MessageDeserializer<?>>() {{
-		put(User.class, new JacksonMessageDeserializer<User>(User.class, objectMapper));
-		put(Device.class, new JacksonMessageDeserializer<Device>(Device.class, objectMapper));
-	}};
-	
 	/**
 	 * Default constructor
 	 */
@@ -86,19 +42,11 @@ public class ElasterixServer implements ActorSystemConfiguration, ActorSystemBoo
 		this("ElasterixServer", 2);
 	}
 	public ElasterixServer(String name, int numberOfShards) {
+        super("elasterix-server-beans.xml");
 		this.name = name;
 		this.numberOfShards = numberOfShards;
 	}
 
-	@Override
-	public void initialize(ActorSystem actorSystem) throws Exception {
-		// register jackson module for Actor serialization
-		objectMapper.registerModule(
-				new SimpleModule("ElasterixModule",new Version(0,1,0,"SNAPSHOT"))
-				.addSerializer(ActorRef.class, new JacksonActorRefSerializer())
-				.addDeserializer(ActorRef.class, new JacksonActorRefDeserializer(
-						actorSystem.getParent().getActorRefFactory())));
-	}
 
 	@Override
 	public String getName() {
@@ -115,40 +63,19 @@ public class ElasterixServer implements ActorSystemConfiguration, ActorSystemBoo
 		return getClass().getPackage().getImplementationVersion();
 	}
 
-	@Override
-	public <T> MessageSerializer<T> getSerializer(Class<T> messageClass) {
-		return (MessageSerializer<T>) messageSerializers.get(messageClass);
-	}
+    @Override
+    protected void doInitialize(ApplicationContext applicationContext, ActorSystem actorSystem) {
 
-	@Override
-	public <T> MessageDeserializer<T> getDeserializer(Class<T> messageClass) {
-		return (MessageDeserializer<T>) messageDeserializers.get(messageClass);
-	}
+    }
 
-	@Override
-	public Serializer<ActorState, byte[]> getActorStateSerializer() {
-		return actorStateSerializer;
-	}
-
-	@Override
-	public Deserializer<byte[], ActorState> getActorStateDeserializer() {
-		return actorStateDeserializer;
-	}
-
-	@Override
+    @Override
 	public void create(ActorSystem actorSystem, String... strings) throws Exception {
-		log.info(String.format("create. [%s]", StringUtils.arrayToCommaDelimitedString(strings)));
-		// Construct message handler (we need it with an actorSystem attached
-		sipMessageHandler = new SipMessageHandlerImpl(actorSystem);
+
 	}
 
 	@Override
 	public void activate(ActorSystem actorSystem) throws Exception {
 		log.info(String.format("activate."));
 	}
-	@Override
-	public ElasticActor getService(String serviceId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 }
